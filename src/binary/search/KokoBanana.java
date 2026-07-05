034677a147aaa4c22d788b73af5ee4f1bcc5f499
chaitanyamahajan <chaitanya.mahajan13@gmail.com>
2026-07-06 00:03:03 +0530

    initial commit

---
+++ src/binary/search/KokoBanana.java
@@ -0,0 +1,41 @@
+package binary.search;
+
+public class KokoBanana {
+
+    public static void main(String[] args) {
+        int[] piles = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int hours = 15;
+        int minEatingSpeed = minHours(piles, hours);
+        System.out.println(minEatingSpeed);
+    }
+
+    public static boolean isPossible(int[] piles, int k, int maxHours) {
+        int hours = 0;
+        for (int i = 0; i < piles.length; i++) {
+            hours += (piles[i] + k - 1) / k;
+        }
+        return maxHours >= hours;
+    }
+
+    public static int minHours(int[] piles, int maxHours) {
+        if (piles == null || piles.length == 0) {
+            return 0;
+        }
+        int left = 1;
+        int right = 0;
+        for (int pile : piles) {
+            right = Math.max(right, pile);
+        }
+        int minHours = right;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (isPossible(piles, mid, maxHours)) {
+                minHours = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return minHours;
+    }
+}
